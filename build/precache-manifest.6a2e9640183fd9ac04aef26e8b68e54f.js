c02cc8f582aa494325aea6b835f6c70fa6a87638
nickman8 <57720318+nickman8@users.noreply.github.com>
2020-10-06 15:46:23 +0300

    Add files via upload

---
+++ build/precache-manifest.6a2e9640183fd9ac04aef26e8b68e54f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163d436f589d80738280d5ea8ed0ac31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e70dfc70f05d23aeeed8",
+    "url": "/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "d1314a1a125f309bf533",
+    "url": "/static/css/main.55feb286.chunk.css"
+  },
+  {
+    "revision": "e70dfc70f05d23aeeed8",
+    "url": "/static/js/2.eedc7626.chunk.js"
+  },
+  {
+    "revision": "b5b05b221331b0b79c425331f756ccfc",
+    "url": "/static/js/2.eedc7626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1314a1a125f309bf533",
+    "url": "/static/js/main.ae47b74e.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
